61af84889b327c9a94e3bc4e90b87f6dbd4b5b55
Emon Ghosh <emonghosh001@gmail.com>
2025-08-20 14:45:25 +0600

    Initial commit after pulling changes

---
+++ Backend/gibhli-art/src/test/java/com/gibhliArt/GibhliArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gibhliArt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GibhliArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
